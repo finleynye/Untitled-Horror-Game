db49a959ca7fca7f78db9e4a4564960f890f6f13
Finley Nye <49366542+finleynye@users.noreply.github.com>
2026-06-16 11:59:21 +0100

    Stamina Wheel Update

--- Assets/Project/Scripts/UI/Player/PlayerInteractionPromptUI.cs
+++ Assets/Project/Scripts/UI/Player/PlayerInteractionPromptUI.cs
@@ -10,7 +10,6 @@ public class PlayerInteractionPromptUI : MonoBehaviour
 
     private void Start()
     {
-        EnsurePromptReferences();
         HidePrompt();
 
         if (playerInteraction == null)
@@ -20,7 +19,6 @@ public class PlayerInteractionPromptUI : MonoBehaviour
 
     private void Update()
     {
-        EnsurePromptReferences();
 
         if (playerInteraction != null && (!playerInteraction.isActiveAndEnabled || !playerInteraction.isOwned))
             playerInteraction = null;
@@ -89,44 +87,4 @@ public class PlayerInteractionPromptUI : MonoBehaviour
             promptText.text = "";
     }
 
-    private void EnsurePromptReferences()
-    {
-        if (promptPanel != null && promptText != null)
-            return;
-
-        if (promptPanel == null)
-        {
-            promptPanel = new GameObject("Interaction Prompt", typeof(RectTransform));
-            promptPanel.transform.SetParent(transform, false);
-
-            RectTransform promptRect = promptPanel.GetComponent<RectTransform>();
-            promptRect.anchorMin = new Vector2(0.5f, 0.5f);
-            promptRect.anchorMax = new Vector2(0.5f, 0.5f);
-            promptRect.anchoredPosition = new Vector2(0f, -160f);
-            promptRect.sizeDelta = new Vector2(520f, 80f);
-        }
-
-        if (promptText == null)
-        {
-            promptText = promptPanel.GetComponentInChildren<TMP_Text>(true);
-
-            if (promptText == null)
-            {
-                GameObject textObject = new GameObject("Prompt Text", typeof(RectTransform), typeof(TextMeshProUGUI));
-                textObject.transform.SetParent(promptPanel.transform, false);
-
-                RectTransform textRect = textObject.GetComponent<RectTransform>();
-                textRect.anchorMin = Vector2.zero;
-                textRect.anchorMax = Vector2.one;
-                textRect.offsetMin = Vector2.zero;
-                textRect.offsetMax = Vector2.zero;
-
-                promptText = textObject.GetComponent<TextMeshProUGUI>();
-                promptText.alignment = TextAlignmentOptions.Center;
-                promptText.fontSize = 28f;
-                promptText.color = Color.white;
-                promptText.raycastTarget = false;
-            }
-        }
-    }
 }
--- Assets/Project/Scripts/UI/Stamina/PlayerStaminaUI.cs
+++ Assets/Project/Scripts/UI/Stamina/PlayerStaminaUI.cs
@@ -5,49 +5,82 @@ using Mirror;
 
 public class PlayerStaminaUI : MonoBehaviour
 {
+    [Header("Refs")]
     public PlayerStamina playerStamina;
-    public Image wheelRed, wheelGreen;
+    public Image wheelBackground;
+    public Image wheelFill;
     public bool showStaminaText;
+
+    [Header("Stamina Colours")]
+    public Color normalColour = new Color32(125, 207, 160, 255);   
+    public Color lowColour = new Color32(214, 184, 90, 255);       
+    public Color criticalColour = new Color32(196, 91, 91, 255); 
+
+    [Header("Colour Settings")]
+    [Range(0f, 1f)] public float lowStaminaPoint = 0.5f;
+    [Range(0f, 1f)] public float criticalStaminaPoint = 0.2f;
+    public float colourLerpSpeed = 8f;
+
+    [Header("Canvas")]
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private float canvasDistanceFromCamera = 0.2f;
 
     void Start()
     {
+        if (canvas == null)
+            canvas = GetComponent<Canvas>();
 
-        if(playerStamina == null)
+        if (playerStamina == null)
             FindLocalPlayerStamina();
+
+        FindLocalPlayerCamera();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (playerStamina == null || !playerStamina.isActiveAndEnabled)
         {
             FindLocalPlayerStamina();
+            FindLocalPlayerCamera();
             return;
         }
 
         UpdateStaminaUI();
     }
+
     private void UpdateStaminaUI()
     {
         if (playerStamina == null)
             return;
 
         float maxStamina = Mathf.Max(1f, playerStamina.MaxStamina);
+        float staminaPercent = Mathf.Clamp01(playerStamina.CurrentStamina / maxStamina);
 
-        if (wheelGreen != null)
-            wheelGreen.fillAmount = playerStamina.CurrentStamina / maxStamina;
-
-        if(playerStamina.currentStamina > 1 && playerStamina.isUsingStamina)
+        if (wheelFill != null)
         {
-            if (wheelRed != null)
-                wheelRed.fillAmount = Mathf.Clamp01((playerStamina.CurrentStamina + 5f) / maxStamina);
-        }
-        else
-        {
-            if (wheelRed != null)
-                wheelRed.fillAmount = Mathf.MoveTowards(wheelRed.fillAmount, 0f, Time.deltaTime);
+            wheelFill.fillAmount = staminaPercent;
+            wheelFill.color = Color.Lerp(wheelFill.color, GetStaminaColour(staminaPercent), Time.deltaTime * colourLerpSpeed);
         }
 
+        if (wheelBackground != null)
+        {
+            wheelBackground.fillAmount = 1f;
+        }
+    }
+
+    private Color GetStaminaColour(float staminaPercent)
+    {
+        if (staminaPercent <= criticalStaminaPoint)
+            return criticalColour;
+
+        if (staminaPercent <= lowStaminaPoint)
+        {
+            float colourBlend = Mathf.InverseLerp(criticalStaminaPoint, lowStaminaPoint, staminaPercent);
+            return Color.Lerp(criticalColour, lowColour, colourBlend);
+        }
+
+        float normalColourBlend = Mathf.InverseLerp(lowStaminaPoint, 1f, staminaPercent);
+        return Color.Lerp(lowColour, normalColour, normalColourBlend);
     }
 
     private void FindLocalPlayerStamina()
@@ -79,9 +112,40 @@ public class PlayerStaminaUI : MonoBehaviour
             if (identity != null && (identity.isLocalPlayer || identity.isOwned))
             {
                 playerStamina = stamina;
-
                 return;
             }
         }
+    }
+
+    private void FindLocalPlayerCamera()
+    {
+        if (canvas == null)
+            canvas = GetComponent<Canvas>();
+
+        if (canvas == null)
+            return;
+
+        GameObject roleObject = GetLocalRoleObject();
+
+        if (roleObject == null)
+            return;
+
+        Camera localCamera = roleObject.GetComponentInChildren<Camera>(true);
+
+        if (localCamera == null)
+            return;
+    }
+
+    private GameObject GetLocalRoleObject()
+    {
+        if (NetworkClient.localPlayer == null)
+            return null;
+
+        PlayerController playerController = NetworkClient.localPlayer.GetComponent<PlayerController>();
+
+        if (playerController == null)
+            return null;
+
+        return playerController.GetCurrentRoleObject();
     }
 }
